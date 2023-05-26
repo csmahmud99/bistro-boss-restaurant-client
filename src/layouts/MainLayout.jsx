512da7e775077fb6ae757ee161cf394e242e8e9a
csmahmud99 <csmahmud.2021@gmail.com>
2023-05-27 01:28:37 +0600

    Basic Footer Done from DaisyUI

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from 'react-router-dom';
+import Footer from '../Pages/Shared/Footer/Footer';
 
 const MainLayout = () => {
     return (
         <div>
-            <Outlet />
+            <div className="max-w-7xl mx-auto">
+                <Outlet />
+            </div>
+            <Footer />
         </div>
     );
 };
