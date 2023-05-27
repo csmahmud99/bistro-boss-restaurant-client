1f89f7f28287b1ae22453fb66414aacb62092546
csmahmud99 <csmahmud.2021@gmail.com>
2023-05-27 11:31:23 +0600

    Header Navigation Added

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Footer from '../Pages/Shared/Footer/Footer';
+import NavigationBar from '../Pages/Shared/NavigationBar/NavigationBar';
 
 const MainLayout = () => {
     return (
         <div>
+            <NavigationBar />
             <div className="max-w-7xl mx-auto">
                 <Outlet />
             </div>
