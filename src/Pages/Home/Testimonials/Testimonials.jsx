0b66d21af537a3090f4630ec31887000d9223933
csmahmud99 <csmahmud.2021@gmail.com>
2023-05-30 21:35:36 +0600

    Testimonials Section at Home Page --- Done

--- src/Pages/Home/Testimonials/Testimonials.jsx
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -1,4 +1,5 @@
 import SectionTitle from "../../Shared/SectionTitle/SectionTitle";
+import { useEffect, useState } from "react";
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -10,7 +11,24 @@ import "swiper/css/navigation";
 // import required modules
 import { Navigation } from "swiper";
 
+// React Rating
+import { Rating } from "@smastrom/react-rating";
+import '@smastrom/react-rating/style.css';
+
+// React Icons
+import { FaQuoteLeft } from 'react-icons/fa';
+
+
+
 const Testimonials = () => {
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        fetch("reviews.json")
+            .then(res => res.json())
+            .then(data => setReviews(data));
+    }, [])
+
     return (
         <div>
             <section>
@@ -21,16 +39,23 @@ const Testimonials = () => {
             </section>
 
             <div>
-                <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-                    <SwiperSlide>Slide 1</SwiperSlide>
-                    <SwiperSlide>Slide 2</SwiperSlide>
-                    <SwiperSlide>Slide 3</SwiperSlide>
-                    <SwiperSlide>Slide 4</SwiperSlide>
-                    <SwiperSlide>Slide 5</SwiperSlide>
-                    <SwiperSlide>Slide 6</SwiperSlide>
-                    <SwiperSlide>Slide 7</SwiperSlide>
-                    <SwiperSlide>Slide 8</SwiperSlide>
-                    <SwiperSlide>Slide 9</SwiperSlide>
+                <Swiper navigation={true} modules={[Navigation]} className="mySwiper" centeredSlides={true}>
+                    {
+                        reviews.map(review => <SwiperSlide
+                            key={review._id}
+                            review={review}>
+                            <div className="flex flex-col items-center text-center space-y-4 px-20">
+                                <FaQuoteLeft className="text-4xl" />
+                                <Rating
+                                    style={{ maxWidth: 180 }}
+                                    value={review.rating}
+                                    readOnly
+                                />
+                                <p>{review.details}</p>
+                                <p className="text-[#CD9003] text-xl font-bold mt-4">{review.name}</p>
+                            </div>
+                        </SwiperSlide>)
+                    }
                 </Swiper>
             </div>
         </div>
