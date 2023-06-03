1ebbb3fa33fbec4c209419cea9b8abfc533f859b
csmahmud99 <csmahmud.2021@gmail.com>
2023-06-04 00:34:17 +0600

    onAuthStateChanged --- Added

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,16 +1,27 @@
-import { createContext, useState } from "react";
-import { getAuth } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
 
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            console.log("Current user:", currentUser);
+            setLoading(false);
+        });
+        return () => {
+            return unsubscribe();
+        };
+    }, []);
+
     const AuthInfo = {
-        user, 
+        user,
         loading
     };
 
